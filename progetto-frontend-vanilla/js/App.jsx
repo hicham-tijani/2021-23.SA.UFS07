31058e6b44300381f94ab48f61fcf6026f5847ec
Tijani <h.tijani@itsrizzoli.it>
2022-05-31 10:47:14 +0200

    feat: add button

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button} from "test-1-react/atoms/button/Button"
+import {Button} from "test-1-react-hicham/atoms/button/Button"
 
 const App = () => ( 
     <div>
